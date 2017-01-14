519b112575696f85708dd0312e01a15cb44f5769
Apps Git <zumry@gapstars.net>
2017-01-14 22:02:05 +0530

    Initial Commit

---
+++ MyGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGitTest
+//
+//  Created by Zumry Gapstars on 1/14/17.
+//  Copyright © 2017 Appsgit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyGitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGitTest
+//
+//  Created by Zumry Gapstars on 1/14/17.
+//  Copyright © 2017 Appsgit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
